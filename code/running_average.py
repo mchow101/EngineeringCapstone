e58bf5e392859975d6409e36fb59431ce9fd029b
mchow101 <mintychocolate101@gmail.com>
2019-11-15 09:09:23 -0800

    Running average

--- code/running_average.py
+++ code/running_average.py
@@ -1,8 +1,12 @@
-lst = []
 def average_init(a):
+  global lst
   lst = a
+  print (lst)
  
 def update(n):
+  global lst
   lst.append(lst.pop(0))
   lst[len(lst) - 1] = n
   return sum(lst)/len(lst)
+
+lst = []
