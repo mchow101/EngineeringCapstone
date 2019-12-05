bf0fd71dbf0d5423da225806bb5e1affd80022d1
mchow101 <mintychocolate101@gmail.com>
2019-12-05 14:29:20 -0800

    update

--- code/ultrasonic_distance.py
+++ code/ultrasonic_distance.py
@@ -6,14 +6,15 @@ import time
 GPIO.setmode(GPIO.BCM)
 #GPIO_TRIG = 17
 #GPIO_ECHO = 27
-GPIO_TRIG = 9
-GPIO_ECHO = 11
+#GPIO_TRIG = 9
+#GPIO_ECHO = 11
 
-GPIO.setup(GPIO_TRIG, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+def init(GPIO_TRIG, GPIO_ECHO):
+        GPIO.setup(GPIO_TRIG, GPIO.OUT)
+        GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 #get distance
-def distance():
+def distance(GPIO_TRIG, GPIO_ECHO):
 	#send trigger pulse
 	GPIO.output(GPIO_TRIG, True)
 	time.sleep(0.00001)
@@ -35,12 +36,20 @@ def distance():
 	return distance
 
 if __name__ == '__main__':
+        trig1 = 17
+        echo1 = 27
+        trig2 = 9
+        echo2 = 11
 	try:
+            init(trig1, echo1)
+            init(trig2, echo2)
             dist_sum = 0
             while True:
-                dist = distance()
+                dist1 = distance(trig1, echo1)
+                dist2 = distance(trig2, echo2)
                 time.sleep(1)
-                print ("Distance = %.3f cm" % dist)
+                print ("Distance 1 = %.3f cm" % dist1)
+                print ("Distance 2 = %.3f cm" % dist2)
         except KeyboardInterrupt:
             print ("Measurement stopped")
             GPIO.cleanup()
