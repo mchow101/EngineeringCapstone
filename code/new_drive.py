8b135302ff866979271c870d7e32e74610a1f7b3
unknown <mintychocolate101@gmail.com>
2020-01-07 22:31:41 -0800

    update

--- code/new_drive.py
+++ code/new_drive.py
@@ -13,14 +13,16 @@ echo2 = 11
 trig3 = 19
 echo3 = 26
 
-FORWARD_SPEED = MAX_SPEED * 3 / 4
+FORWARD_SPEED = MAX_SPEED * 5 / 8
 REVERSE_SPEED = FORWARD_SPEED # MAX_SPEED / 2
-ACCELERATE = list(range(100, FORWARD_SPEED, 10)) + [FORWARD_SPEED]
+# ACCELERATE = list(range(100, FORWARD_SPEED, 10)) + [FORWARD_SPEED]
 ACC = True
 DEC = False
-DEC_VALUE = -18
-DECELERATE = list(range(FORWARD_SPEED, 0, DEC_VALUE)) + [0] 
-index = 0
+STOP = False
+ACC_VALUE = 10
+STOP_VALUE = 20
+Lspeed = 0
+Rspeed = 0
 
 MIN_DIST = 60
 WINDOW_LENGTH = 5
@@ -38,7 +40,6 @@ if __name__ == '__main__':
 #	ultrasonic_distance.init(trig2, echo2)
 #	ultrasonic_distance.init(trig3, echo3)
         while True:
-#	    dist = ultrasonic_distance.distance(trig1, echo1)
             dist = running_average.update(ultrasonic_distance.distance(trig1, echo1))
 #	    left = running_average.update(ultrasonic_distance.distance(trig2, echo2))
 #	    right = running_average.update(ultrasonic_distance.distance(trig3, echo3))
@@ -46,58 +47,47 @@ if __name__ == '__main__':
             if dist < MIN_DIST and DEC == False:
 		print ("TOO CLOSE")
                 DEC = True
-		DECELERATE = list(range(Lspeed, -REVERSE_SPEED, DEC_VALUE)) + list(range(-REVERSE_SPEED, 0, -DEC_VALUE))
-#		L_DECELERATE = list(range(Lspeed, -REVERSE_SPEED, DEC_VALUE)) + list(range(-REVERSE_SPEED, 0, -DEC_VALUE))
-#		R_DECELERATE = list(map(lambda x: Rspeed if 
-#		R_DECELERATE = list(map(lambda x: 0 if x < 0 else x, L_DECELERATE))
                 ACC = False
-		print (DECELERATE)
-                index = 0
 	    # start if in range
 	    elif dist >= MIN_DIST and ACC == False:
 		ACC = True
-		index = 0
+                DEC = False
             # reached max speed
-            elif ACC == True and (Lspeed == FORWARD_SPEED or Rspeed == FORWARD_SPEED):
-		print ("CRUISING")
+            elif ACC == True and (Lspeed >= FORWARD_SPEED or Rspeed >= FORWARD_SPEED):
+		print ("CRUISING FORWARD")
                 ACC = False
                 Lspeed = FORWARD_SPEED
                 Rspeed = FORWARD_SPEED
-                index = 0
+            elif DEC == True and (Lspeed <= -REVERSE_SPEED or Rspeed <= -REVERSE_SPEED):
+		print ("CRUISING REVERSE")
+                DEC = False
+                Lspeed = -REVERSE_SPEED
+                Rspeed = -REVERSE_SPEED
             # stopped
-#            elif DEC == True and (Lspeed == 0 or Rspeed == 0):
-#                print ("STOPPED")
-#		DEC = False
-#                Lspeed = 0
-#                Rspeed = 0
-#                index = 0
+            elif STOP == True and (Lspeed == 0 or Rspeed == 0):
+                print ("STOPPED")
+		STOP = False
+                Lspeed = 0
+                Rspeed = 0
             # accelerate
             elif ACC == True:
 		print ("ACCEERLERLER")
-                Lspeed = ACCELERATE[index]
-                Rspeed = ACCELERATE[index]
-                index = index + 1 
+                Lspeed = Lspeed + ACC_VALUE
+                Rspeed = Rspeed + ACC_VALUE
             # decelerate
             elif DEC == True:
 		print ("DECELERKERLKEJR")
-                Lspeed = DECELERATE[index]
-                Rspeed = DECELERATE[index]
-#		Lspeed = L_DECELERATE[index]
-#		Rspeed = R_DECELERATE[index]
-                index = index + 1
-		if index >= len(DECELERATE) :
-		    index = len(DECELERATE) - 1
+                Lspeed = Lspeed - ACC_VALUE
+                Rspeed = Rspeed - ACC_VALUE
             # cruise
-	    elif DEC == False and ACC == False and Rspeed > 0 or Lspeed > 0:
+	    elif DEC == False and ACC == False and STOP == False and (Rspeed > 0 or Lspeed > 0):
 		print ("ACTUALLY CRUISING")
 		Lspeed = FORWARD_SPEED
 		Rspeed = FORWARD_SPEED
-		index = 0
             else:
 		print ("E STOPPED")
                 Lspeed = 0
 		Rspeed = 0
-                index = 0
                 
             # set speed
             motors.setSpeeds(-Lspeed, Rspeed)
